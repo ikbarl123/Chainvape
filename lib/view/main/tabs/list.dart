b3c70d0796cc554998cd5520d5835bdc441d9662
ikbarl123 <ikbarl123@gmail.com>
2022-01-06 16:47:24 +0700

    staring templateing ui

---
+++ lib/view/main/tabs/list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListStore extends StatefulWidget {
+  const ListStore({ Key? key }) : super(key: key);
+
+  @override
+  _ListStoreState createState() => _ListStoreState();
+}
+
+class _ListStoreState extends State<ListStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/view/main/tabs/setting.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingTabs extends StatefulWidget {
+  const SettingTabs({ Key? key }) : super(key: key);
+
+  @override
+  _SettingTabsState createState() => _SettingTabsState();
+}
+
+class _SettingTabsState extends State<SettingTabs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
