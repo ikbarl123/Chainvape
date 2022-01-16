8aba9145294c1f08bf3ba5323e5894e5fbea4fb3
ikbarl123 <ikbarl123@gmail.com>
2022-01-16 18:23:33 +0700

    moved app url

--- lib/service/appurl.dart
+++ lib/service/appurl.dart
@@ -1,9 +1,9 @@
 class AppUrl {
 
-  static const String url="http://127.0.0.1";
+  static const String url="http://10.0.2.2:8000";
   static const String login=url+"/api/auth/login";
   static const String logout=url+"/api/auth/logout";
-  static const String me=url+"/api/me";
+  static const String me=url+"/vapestore";
   static const String register=url+"/register";
   //static const String apiStore="/store";
   //static const String apiFavorite="/fav";
