c6ac6bdec16692d2d6e9ceea153cf423af49120c
ikbarl123 <ikbarl123@gmail.com>
2022-01-17 21:58:06 +0700

    minor fix

--- lib/service/storeservice.dart
+++ lib/service/storeservice.dart
@@ -1,13 +1,10 @@
 part of 'service.dart';
 
-
-
-
 class StoreService {
 
   List<Vapestore> storeList = [];
   Future<List<Vapestore>> getStoreList() async{
-    final response = await http.get(Uri.parse(AppUrl.me));
+    final response = await get(Uri.parse(AppUrl.me));
     var data = jsonDecode(response.body.toString());
     if(response.statusCode==200){
       for(Map i in data){
