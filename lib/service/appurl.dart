45fe3bb55db3c58251227967031a29d457980fcb
ikbarl123 <ikbarl123@gmail.com>
2022-02-02 06:11:56 +0700

    beter url pathing

--- lib/service/appurl.dart
+++ lib/service/appurl.dart
@@ -2,15 +2,15 @@ part of 'service.dart';
 
 class AppUrl {
 
-  static const String url="http://10.0.2.2:8000";
-  static const String login=url+"/api/auth/login";
-  static const String logout=url+"/api/auth/logout";
-  static const String register=url+"/api/auth/register";
-  static const String store=url+"/api/vapestore/get";
-  static const String forum=url+"/api/forum";
-  static const String thread=url+"/api/thread/get";
-  static const String post=url+"/api/thread/create";
-  static const String reply=url+"/api/thread/reply";
+  static const String url="http://10.0.2.2:8000/api";
+  static const String login=url+"/auth/login";
+  static const String logout=url+"/auth/logout";
+  static const String register=url+"/auth/register";
+  static const String store=url+"/vapestore/get";
+  static const String forum=url+"/forum";
+  static const String thread=url+"/thread/get/";
+  static const String post=url+"/thread/create";
+  static const String reply=url+"/thread/reply";
   //static const String apiStore="/store";
   //static const String apiFavorite="/fav";
 }
