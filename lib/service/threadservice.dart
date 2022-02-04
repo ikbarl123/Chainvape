03c223736045f87edf52ed2cc7127fb664b37001
ikbarl123 <ikbarl123@gmail.com>
2022-02-04 17:27:08 +0700

    implemetation of create post

--- lib/service/threadservice.dart
+++ lib/service/threadservice.dart
@@ -30,8 +30,9 @@ class ThreadService {
   Future<List<Reply>> getReplies(int id) async{
     try{
     final response = await get(Uri.parse(AppUrl.thread+"$id"));
-    var data = jsonDecode(response.body.toString());
+    
     if(response.statusCode==200){
+      var data = jsonDecode(response.body.toString());
       for(Map i in data){
         print(response.body);
         repliesList.add(Reply.fromJson(i));
@@ -46,7 +47,7 @@ class ThreadService {
     }
   }
 
-  Future CreatePost(int _id, String _title, String _text,) async {
+  Future CreatePost(String _title, String _text,) async {
       Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   
   try{
@@ -54,43 +55,56 @@ class ThreadService {
   final String token = local.getString("token")??"kosong";
   final response = await post(Uri.parse(AppUrl.post),
   headers: {
-    'Authorization': 'Bearer $token',
+    'Authorization': 'Bearer ${token.toString()}',
     'Accept': 'application/json'
     },
   body: {
     'title':_title,
     'text':_text,
   });
- var data = jsonDecode(response.body.toString());
-  print(data);
 
-  //return User.fromJson(data);
+  var data = jsonDecode(response.body.toString());
+     print(response.statusCode);
+  if(response.statusCode==201){
+    print(data);
+      var thread = Thread.fromJson(data[0]);
+      return thread;
+   }else 
+   return {"Unkown error"};
+
   }catch(e){
-    return e;
-  }
-}
+    print(e);
+    }
+    }
 
-Future CreateReply(int _postID,int _id, String _text,) async {
+Future CreateReply(int _postID, String _text,) async {
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  
+  List<Reply> replies =[];
   try{
     final SharedPreferences local = await _prefs;
   final String token = local.getString("token")??"kosong";
   final response = await post(Uri.parse(AppUrl.reply),
   headers: {
-    'Authorization': 'Bearer $token',
+    'Authorization': 'Bearer ${token.toString()}',
     'Accept': 'application/json'
     },
   body: {
-    'post_id':_postID,
-    'password':_text,
+    'post_id':_postID.toString(),
+    'text':_text,
   });
- var data = jsonDecode(response.body.toString());
-  print(data);
+     var data = jsonDecode(response.body.toString());
+     print(response.statusCode);
+ if(response.statusCode==201){
+   dynamic json = jsonDecode(response.body.toString());
+      var reply = Reply.fromJson(json[0]);
+      print(reply);
+      replies.add(reply);
+      return replies;
+   }else 
+   return {"Unkown error"};
 
- // return User.fromJson(data);
   }catch(e){
-    return e;
-  }
-}
-}
+    print(e);
+    throw e;}
+    }
+    }
