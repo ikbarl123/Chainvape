b86c7ee4594923dc7b486a74af1cc60ed3309611
ikbarl123 <ikbarl123@gmail.com>
2022-02-05 07:13:00 +0700

    removed unused import

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -6,11 +6,10 @@ import 'dart:async';
 
 
 import 'package:http/http.dart';
-import 'package:location/location.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 //model
-import 'package:chainvape/model/userlocation.dart';
+
 import 'package:chainvape/model/vapestore.dart';
 import 'package:chainvape/model/thread.dart';
 import 'package:chainvape/model/reply.dart';
@@ -18,7 +17,6 @@ import 'package:chainvape/model/user.dart';
 
 //children
 part 'appurl.dart';
-part 'locationservice.dart';
 part 'storeservice.dart';
 part 'authservice.dart';
 part 'threadservice.dart';
